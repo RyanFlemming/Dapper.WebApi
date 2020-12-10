69e09153e2a89f14d49caf13d84d2f5acbda15be
Ryan Flemming <rmf919@live.com>
2020-12-10 15:08:40 +1100

    Register interfaces with concrete implementations

---
+++ Dapper.Infrastructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Dapper.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dapper.Infrastructure.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IProductRepository Products { get; }
+        public UnitOfWork(IProductRepository productRepository)
+        {
+            Products = productRepository;
+        }
+    }
+}
